f6998426f19af99948431aca4460708090e21318
Pratham1353 <patelpratham799@gmail.com>
2024-01-24 20:34:10 +0530

    Occurence of any Number

--- P1.java
+++ P1.java
@@ -8,9 +8,15 @@ class P1{
         // int c = sc.nextInt();
         // System.out.println(max(a,b,c));
 
-        System.out.print("Enter the number:");
+        // System.out.print("Enter the number:");
+        // int n = sc.nextInt();
+        // fibonacci(n);
+
+        System.out.print("Enter any number:");
         int n = sc.nextInt();
-        fibonacci(n);
+        System.out.print("Enter number you want find occurence:");
+        int a = sc.nextInt();
+        occurence(n,a);
     }
     // public static int max(int a,int b, int c){
     //     if(a>b){
@@ -30,16 +36,28 @@ class P1{
     //         }
     //     }
     // }
-    public static void fibonacci(int n){
-        int FT = 0;
-        int ST = 1;
-        int NT;
+    // public static void fibonacci(int n){
+    //     int FT = 0;
+    //     int ST = 1;
+    //     int NT;
+    //     while(n>0){
+    //         System.out.println(FT);
+    //         NT = FT + ST;
+    //         FT = ST;
+    //         ST = NT;
+    //         n--;
+    //     }
+    // }
+    public static void occurence(int n, int a){
+        int count = 0;
+        int b;
         while(n>0){
-            System.out.println(FT);
-            NT = FT + ST;
-            FT = ST;
-            ST = NT;
-            n--;
+            b = n%10;
+            if(b==a){
+                count++;
+            }
+            n = n/10;
         }
+        System.out.println(count);
     }
 }
