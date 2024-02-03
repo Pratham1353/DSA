59d7c5d23dc51f29b119b7e995f38690b9f7bac0
Pratham1353 <patelpratham799@gmail.com>
2024-02-03 18:51:38 +0530

    loop1

--- Condition_and_Loops.java
+++ Condition_and_Loops.java
@@ -2,14 +2,20 @@ import java.util.*;
 public class Condition_and_Loops {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your Sallary:");
-        int sallary = sc.nextInt();
-        if(sallary>=10000){
-            sallary = sallary + 2000;
+        // System.out.println("Enter your Sallary:");
+        // int sallary = sc.nextInt();
+        // if(sallary>=10000){
+        //     sallary += 2000;
+        // }
+        // else{
+        //     sallary += 1000;
+        // }
+        // System.out.println("Sallary with bonus:" + sallary);
+
+        System.out.print("Enter a number:");
+        int num = sc.nextInt();
+        for (int i = 1; i<= num; i++){
+            System.out.println(i);
         }
-        else{
-            sallary = sallary + 1000;
-        }
-        System.out.println("Sallary with bonus:" + sallary);
     }
 }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,32 +1,9 @@
-import java.util.*;
-class P1{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter Three numbers");
-        // int a = sc.nextInt();
-        // int b = sc.nextInt();
-        // int c = sc.nextInt();
-        // System.out.println(max(a,b,c));
-
-        System.out.print("Enter the number:");
-        int n = sc.nextInt();
-        fibonacci(n);
-    }
-    // public static int max(int a,int b, int c){
-    //     if(a>b){
-    //         if(a>c){
-    //             return a;
-    //         }
-    //         else{
-    //             return c;
-    //         }
-    //     }
-    //     else{
-    //         if(b>c){
-    //             return b;
-    //         }
-    //         else{
-    //             return c;
-    //         }
-    //     }
-    // }
+n("Enter your Sallary:");
+        // int sallary = sc.nextInt();
+        // if(sallary>=10000){
+        //     sallary += 2000;
+        // }
+        // else{
+        //     sallary += 1000;
+        // }
+        // System.out.println("Sallary with bonus:" + sallary);
